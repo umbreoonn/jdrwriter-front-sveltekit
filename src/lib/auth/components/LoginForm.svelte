672a8d1c733156906537e1780da947e15e37cc05
pichu <pichu@MacBook-Air-de-Pichu.local>
2022-12-04 13:28:21 +0100

    feat(auth): add login form component with basic style

---
+++ src/lib/auth/components/LoginForm.svelte
@@ -0,0 +1,15 @@
+<form class="auth-form">
+    <input
+            class="auth-text-input"
+            type="text"
+            placeholder="Adresse e-mail"/>
+    <input
+            class="auth-text-input"
+            type="password"
+            placeholder="Mot de passe"/>
+    <button class="auth-submit-button" type="submit"> Me connecter !</button>
+</form>
+
+<style lang="scss">
+  @import "../style/style";
+</style>
